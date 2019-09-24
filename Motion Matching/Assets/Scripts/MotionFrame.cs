4b54d4f42c66736be14e11f9ced7b6b92949f0c2
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-24 10:01:08 +0200

    removed motion frame as a scriptableobject

--- Motion Matching/Assets/Scripts/MotionFrame.cs
+++ Motion Matching/Assets/Scripts/MotionFrame.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MotionFrame : ScriptableObject
+public class MotionFrame  
 {
     public MotionJointPoint Root;
     public List<MotionJointPoint> EnderPoints = new List<MotionJointPoint>();
