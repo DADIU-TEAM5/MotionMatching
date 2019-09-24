690284aaf7597736b821e1d8750696d2f9bcac86
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-24 09:43:05 +0200

    Added new objects to hold motion data

---
+++ Motion Matching/Assets/Scripts/MotionClip.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionClip : ScriptableObject
+{
+    public MotionJointPoint Root;
+    public List<MotionJointPoint> EnderPoints = new List<MotionJointPoint>();
+
+    public AnimationFrame AnimationFrame;
+}
