db9db8ce7683ad0a80c0213cd1eaa158ebe89af6
Oliver Jørgensen <OliverJ190196@gmail.com>
2019-09-25 15:14:03 +0200

    Created the player input asset

---
+++ Motion Matching/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerInput : ScriptableObject
+{
+    public float Velocity;
+    public float Acceleration;
+    public float Brake;
+    public float Turn;
+    public bool Crouch;
+    public bool Jump;
+
+
+    public Vector3 Direction;
+    public float AngularVelocity;
+
+    public float m_TurnSpeed;
+    public float m_ForwardAmount;
+}
