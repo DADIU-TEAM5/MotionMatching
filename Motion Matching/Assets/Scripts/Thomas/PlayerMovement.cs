3d46ea2176ead63c0038941c5de0d78d1391b4f3
nufanDK <t_a_p@hotmail.com>
2019-10-11 16:02:50 +0200

    another try for future prediction

--- Motion Matching/Assets/Scripts/Thomas/PlayerMovement.cs
+++ Motion Matching/Assets/Scripts/Thomas/PlayerMovement.cs
@@ -27,7 +27,7 @@ public class PlayerMovement : MonoBehaviour
     public Vector3VariableList currentPosition;
     public Vector3VariableList historyPosition;
 
-    //public Vector3 predictedPoint;
+    public GameObject predictedTarget;
     public Vector3[] predictedPointTemp;
 
     public float offsetY = -0.01f;
@@ -47,6 +47,7 @@ public class PlayerMovement : MonoBehaviour
     private Transform _groundChecker;
 
     public Vector3 _prevPosition;
+    private Vector3 inputVel;
 
     // Start is called before the first frame update
     void Start()
@@ -79,7 +80,8 @@ public class PlayerMovement : MonoBehaviour
         _inputs.z = Input.GetAxis("Vertical");
 
         transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * RotationSpeed);
-        transform.Translate(_inputs * Speed * Time.fixedDeltaTime);
+        inputVel = _inputs * Speed * Time.deltaTime;
+        transform.Translate(inputVel);
 
         currentPosition.List[0] = transform.position;
 
@@ -87,7 +89,7 @@ public class PlayerMovement : MonoBehaviour
 
         CreateInstPred();
 
-        //testingTarget.transform.position = test.position;
+        //testingTarget.transform.position = test;
 
     }
 
@@ -96,24 +98,30 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 currentPos = currentPosition.List[0];
         Vector3 predictedTransform = currentPos;
+
         //float linearVelocity = _inputs.z;
-        float linearVelocityDecay;
+        //float linearVelocityDecay;
         Vector3 linearVelocity;
         Vector3 _prevZ = currentPos;
+        float prevVel = inputVel.magnitude;
 
-        linearVelocity = transform.position - _prevZ;
+        linearVelocity = transform.position - _prevZ / Time.deltaTime;
         _prevZ = transform.position;
-        
+
+        predictedTarget.transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * RotationSpeed);
+        predictedTarget.transform.Translate(_inputs * linearVelocity.magnitude / 200);
+
+
 
         float elapsedTime = 0.0f;
         const float timeHorizon = 1.0f;
+        const float deltaTime = (1.0f / 30.0f);
 
-        while (elapsedTime<timeHorizon)
+        while (elapsedTime < timeHorizon)
         {
-            predictedTransform += linearVelocity * Time.deltaTime;
+            predictedTransform += linearVelocity * deltaTime;
 
-            linearVelocityDecay = ExponentialDecay(linearVelocity.magnitude, targetLinearVelocity, Time.deltaTime / (timeHorizon - elapsedTime));
-            Debug.Log(linearVelocity);
+            linearVelocity *= ExponentialDecay(prevVel, inputVel.magnitude, deltaTime / (timeHorizon - elapsedTime));
 
             elapsedTime += Time.deltaTime;
         }
@@ -132,9 +140,9 @@ public class PlayerMovement : MonoBehaviour
     {
         if (CoolDownOver && Once && showPred)
         {
-            GameObject tempDot = Instantiate(dotTemp, new Vector3(test.x, 0.01f, test.z), Quaternion.identity);
+            //GameObject tempDot = Instantiate(dotTemp, new Vector3(test.x, 0.01f, test.z), Quaternion.identity);
 
-            dotInstPred.Add(Instantiate(dotPred, transform.position - (transform.up * (transform.position.y + offsetY)), Quaternion.identity));
+            dotInstPred.Add(Instantiate(dotPred, transform.position - (transform.up * (transform.position.y + offsetY)), transform.rotation));
             predictedPosition.List.Add(dotInstPred[maxCountPred].transform.position);
 
             if(dotInstPred.Count > 1)
@@ -164,7 +172,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (CoolDownOver == true && Once == true && showHist)
         {
-            dotInstHist.Add(Instantiate(dotHist, transform.position - (transform.up * (transform.position.y+offsetY)), Quaternion.identity));
+            dotInstHist.Add(Instantiate(dotHist, transform.position - (transform.up * (transform.position.y+offsetY)), Quaternion.FromToRotation(transform.eulerAngles,new Vector3(0,90,90))));
 
             historyPosition.List.Add(dotInstHist[maxCountHist].transform.localPosition);
 
