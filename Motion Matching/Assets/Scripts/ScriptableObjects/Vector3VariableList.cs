154b7b207814de37e5da3f5b2b4dd4bd7605ea77
nufanDK <t_a_p@hotmail.com>
2019-10-11 10:48:44 +0200

    added history trail to the player

---
+++ Motion Matching/Assets/Scripts/ScriptableObjects/Vector3VariableList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Vector3List", menuName = "Stranger Objects/Vector3List", order = 1)]
+public class Vector3VariableList : ScriptableObject
+{
+    public List<Vector3> List;
+}
